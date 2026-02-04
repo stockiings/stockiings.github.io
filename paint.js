82df510c05957e167894ceeafbf28706105adcbd
-Stockings- <Stockiings@proton.me>
2026-02-04 09:55:06 -0500

    Create paint.js
    functionality to paint

---
+++ paint.js
@@ -0,0 +1,48 @@
+const canvas = document.getElementByID("canvas");
+const ctx = canvas.getContext("2d");
+
+let drawing = false;
+let brushcolor = "#000000";
+let brushsize = 5;
+
+// Set defaults
+ctx.lineCap = "round";
+
+// mouse setup
+canvas.addEventListener("mousedown", () => drawing=true);
+canvas.addEventListener("mouseup", () => drawing=false);
+canvas.addEventListener("mouseleave", () => drawing=false);
+
+canvas.addEventListener("mousemove", draw); 
+
+function draw(e) {
+  if(!drawing) return;
+
+  ctx.strokeStyle = brushcolor;
+  ctx.lineWidth = brushsize;
+
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(e.offsetX, e.offsetY);
+}
+
+// color picker
+document.getElementByID("color").addEventListener("change", e => {
+  brushcolor = e.target.value;
+});
+
+// brush size
+document.getElementByID("size").addEventListener("input", e => {
+  brushsize = e.target.value;
+});
+
+// brush size
+document.getElementByID("eraser").addEventListener("click", () => {
+  brushcolor = "#ffffff";
+});
+
+// Clear canvas
+document.getElementById("clear").addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
